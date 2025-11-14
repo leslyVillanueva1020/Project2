fca193f2d278ed93ba2a5b11eb15f19053537909
Sp00der-Man2099 <adrikrenteria@gmail.com>
2025-11-13 16:26:13 -0800

    created MainActivity.java

---
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Adrik Renteria
+ * Date 11/13/2025
+ * Project: Project 2
+ * Abstract: this is the main activity for the project
+ */
+package com.example.project2;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
