b5a8b20c4675f1088c321ef5a51060a5f4ad6939
marissa-benenati <mbenenati95@gmail.com>
2025-12-05 22:08:41 -0800

    More work on database tests for User table

--- app/src/androidTest/java/com/example/project2/UserDAOTest.java
+++ app/src/androidTest/java/com/example/project2/UserDAOTest.java
@@ -2,9 +2,11 @@ package com.example.project2;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import android.content.Context;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -20,6 +22,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Marissa Benenati
@@ -27,7 +31,7 @@ import java.io.IOException;
  * <br>DATE: 12/5/2025
  * <br>ASSIGNMENT: Project 02
  * <br>==========================
- * <br>DESCRIPTION: This file contains tests for the User table.
+ * <br>DESCRIPTION: This file contains tests for the User table in CareerNestDatabase.
  * */
 @RunWith(AndroidJUnit4.class)
 public class UserDAOTest {
@@ -35,6 +39,12 @@ public class UserDAOTest {
     private UserDAO userDAO;
     private CareerNestDatabase db;
     private CareerNestRepository repository;
+    /// ======================================
+    User testUser1;
+    User testUser2;
+    User testUser3;
+    User testUser4;
+    String pswd = "multipass";
 
     @Before
     public void createDb(){
@@ -43,6 +53,11 @@ public class UserDAOTest {
                 //.allowMainThreadQueries()
                 .build();
         userDAO = db.userDAO();
+
+        testUser1 = new User("johndoe", "password");
+        testUser2 = new User("alicesmith", "passwordA");
+        testUser3 = new User("billybob", "passwordB");
+        testUser4 = new User("charliebrown", "passwordC");
     }
 
     @After
@@ -52,11 +67,12 @@ public class UserDAOTest {
 
     /**
      * Insert a single user and retrieve by ID.
+     * @throws Exception
      */
     @Test
     public void insertAndGetUser() throws Exception{
-        User testUser1 = new User("johndoe", "password1");
-        repository.insertUser(testUser1);
+        userDAO.insert(testUser1);
+        //repository.insertUser(testUser1);
         User retrievedUser = userDAO.getUserByUserId(testUser1.getId()).getValue();
 
         assertNotNull(retrievedUser);
@@ -65,5 +81,62 @@ public class UserDAOTest {
         assertEquals("password1", retrievedUser.getPassword());
     }
 
+    /**
+     * Insert multiple users, retrieve all, then check count retrieved.
+     * @throws Exception
+     */
+    @Test
+    public void insertMultUsersAndGetAll() throws Exception{
+        userDAO.insert(testUser2);
+        userDAO.insert(testUser3);
+        userDAO.insert(testUser4);
 
+        LiveData<List<User>> allUsers = userDAO.getAllUsers();
+
+        assertEquals(3, Objects.requireNonNull(allUsers.getValue()).size()); //using requireNonNull according to IDE recommendation
+    }
+
+    /**
+     * Tests <code>UPDATE</code> query. Checks both username and password updates.
+     * @throws Exception
+     */
+    @Test
+    public void updateUser() throws Exception{
+        /// Test for username update
+        userDAO.insert(testUser1);
+        User retrievedUser = userDAO.getUserByUserId(testUser1.getId()).getValue();
+        assertNotNull(retrievedUser);
+        retrievedUser.setUsername("korbendallas");
+        userDAO.update(retrievedUser);
+
+        User updatedUser = userDAO.getUserByUserId(testUser1.getId()).getValue();
+        assertNotNull(updatedUser);
+        assertEquals("korbendallas", updatedUser.getUsername());
+
+
+        /// Test for password update
+        userDAO.insert(testUser4);
+        User retrievedUser2 = userDAO.getUserByUserId(testUser4.getId()).getValue();
+        assertNotNull(retrievedUser2);
+        retrievedUser2.setPassword(pswd);
+        userDAO.update(retrievedUser2);
+
+        User updatedUser2 = userDAO.getUserByUserId(testUser4.getId()).getValue();
+        assertNotNull(updatedUser2);
+        assertEquals("multipass", updatedUser2.getPassword());
+    }
+
+    /**
+     * Tests <code>DELETE</code> query for a single user.
+     * @throws Exception
+     */
+    @Test
+    public void deleteUser() throws Exception{
+        userDAO.insert(testUser2);
+        User retrievedUser = userDAO.getUserByUserId(testUser2.getId()).getValue();
+        assertNotNull(retrievedUser);
+        userDAO.delete(retrievedUser);
+        User deletedUser = userDAO.getUserByUserId(testUser2.getId()).getValue();
+        assertNull(deletedUser);
+    }
 }
