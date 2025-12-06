1c3e57e8d7bd7434b1bce4953a9b01cb7370037d
Sp00der-Man2099 <adrikrenteria@gmail.com>
2025-12-06 13:24:55 -0800

    renamed Reminder file to ReminderActivity.java

--- app/src/main/java/com/example/project2/ReminderActivity.java
+++ app/src/main/java/com/example/project2/ReminderActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Reminder extends AppCompatActivity {
+public class ReminderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
