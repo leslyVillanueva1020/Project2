91a538875c7cc804e44dbb80608f8eb6f49aba85
EstrellaOrtiz-F <eortiz-felix@csumb.edu>
2025-12-06 19:03:41 -0800

    Added the imports.

--- app/src/androidTest/java/com/example/Database/JobLogDaoTest.java
+++ app/src/androidTest/java/com/example/Database/JobLogDaoTest.java
@@ -1,4 +1,23 @@
 package com.example.Database;
 
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.project2.database.entities.JobLog;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
 public class JobLogDaoTest {
 }
