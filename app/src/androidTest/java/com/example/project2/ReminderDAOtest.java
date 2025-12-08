899a9217d6913440593300f6117ea915bf19f806
Sp00der-Man2099 <adrikrenteria@gmail.com>
2025-12-07 21:59:25 -0800

    created the blank file

---
+++ app/src/androidTest/java/com/example/project2/ReminderDAOtest.java
@@ -0,0 +1,11 @@
+package com.example.project2;
+/**
+ * @author Adrik Renteria
+ * <br>COURSE: CST 338 - Software Design
+ * <br>DATE: 12/7/2025
+ * <br>ASSIGNMENT: Project 02
+ * <br>==========================
+ * <br>DESCRIPTION: This file contains tests for the User table in CareerNestDatabase.
+ * */
+public class ReminderDAOtest {
+}
