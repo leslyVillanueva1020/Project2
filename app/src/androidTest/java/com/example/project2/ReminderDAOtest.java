453044715d736439f600c1b7b3a612cf4567af56
Sp00der-Man2099 <adrikrenteria@gmail.com>
2025-12-07 22:07:02 -0800

    added some more code

--- app/src/androidTest/java/com/example/project2/ReminderDAOtest.java
+++ app/src/androidTest/java/com/example/project2/ReminderDAOtest.java
@@ -1,4 +1,16 @@
 package com.example.project2;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.project2.database.CareerNestDatabase;
+import com.example.project2.database.ReminderDAO;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
 /**
  * @author Adrik Renteria
  * <br>COURSE: CST 338 - Software Design
@@ -7,5 +19,16 @@ package com.example.project2;
  * <br>==========================
  * <br>DESCRIPTION: This file contains tests for the User table in CareerNestDatabase.
  * */
+@RunWith(AndroidJUnit4.class)
 public class ReminderDAOtest {
+
+    private CareerNestDatabase db;
+    private ReminderDAO reminderDAO;
+
+    @Before
+    public void setUp(){
+        Context context = ApplicationProvider.getApplicationContext();
+
+    }
+
 }
